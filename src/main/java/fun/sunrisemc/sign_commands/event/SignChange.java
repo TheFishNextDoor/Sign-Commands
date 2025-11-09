4f793b542ce347ab69f1c9e563175ec41fe9f63f
TheFishNextDoor <wonderfulwiz9@gmail.com>
2025-11-09 10:33:00 -0600

    sign change null checks

--- src/main/java/fun/sunrisemc/sign_commands/event/SignChange.java
+++ src/main/java/fun/sunrisemc/sign_commands/event/SignChange.java
@@ -12,12 +12,14 @@ public class SignChange implements Listener {
 
     @EventHandler(ignoreCancelled = true)
     public void onSignChange(@NotNull SignChangeEvent event) {
-        if (!event.getPlayer().hasPermission(Permissions.SIGN_EDIT_COLOR_PERMISSION)) {
-            return;
-        }
-
-        for (int i = 0; i < event.getLines().length; i++) {
-            event.setLine(i, org.bukkit.ChatColor.translateAlternateColorCodes('&', event.getLine(i)));
+        if (event.getPlayer().hasPermission(Permissions.SIGN_EDIT_COLOR_PERMISSION)) {
+            for (int i = 0; i < event.getLines().length; i++) {
+                String line = event.getLine(i);
+                if (line == null) {
+                    continue;
+                }
+                event.setLine(i, org.bukkit.ChatColor.translateAlternateColorCodes('&', line));
+            }
         }
     }
 }
