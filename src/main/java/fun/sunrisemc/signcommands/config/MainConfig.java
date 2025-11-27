c2d4b6ec39bc669e9f049e63113f1bfac9e65d2e
TheFishNextDoor <wonderfulwiz9@gmail.com>
2025-11-26 18:54:01 -0600

    update yaml utils

--- src/main/java/fun/sunrisemc/signcommands/config/MainConfig.java
+++ src/main/java/fun/sunrisemc/signcommands/config/MainConfig.java
@@ -15,6 +15,6 @@ public class MainConfig {
         YamlConfiguration config = ConfigFile.get("config", true);
 
         this.ONLY_ALLOW_SIGNS = config.getBoolean("only-allow-signs", true);
-        this.SIGN_CLICK_DELAY_TICKS = YAMLUtils.getIntClamped(config, "sign-click-delay-ticks", 0, Integer.MAX_VALUE);
+        this.SIGN_CLICK_DELAY_TICKS = YAMLUtils.getIntClamped(config, "sign-click-delay-ticks", 0, Integer.MAX_VALUE).orElse(5);
     }
 }
